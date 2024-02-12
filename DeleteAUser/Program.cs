09f228ca5acd038b84f179e6cee207bb54d77beb
Mohammad Rahman <md.wahidovi@gmail.com>
2024-02-12 12:43:52 -0800

    first commit

---
+++ DeleteAUser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteAUser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter the Username you want to Delete:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"given username, {name}!");
+            Console.ReadKey(); // Waits for a key press before closing the console
+        }
+    }
+}
